eaf7e63d84c09cfdd44ea3ce02641102a75d8310
Rui Neto <ruineto11@gmail.com>
2021-11-10 02:58:17 -0300

    Projeto #05 - Bate-Papo UOL: Front-end concluído

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-//alert("Carreguei o JavaScript!");
+function userOptionsOpen() {
+  let userOptionsBackgroundElement = document.querySelector(".user-options");
+  userOptionsBackgroundElement.classList.add("opacity-show");
+  userOptionsBackgroundElement.classList.remove("opacity-hidden");
+
+  let userOptionsElement = document.querySelector(".user-options-content-general");
+  userOptionsElement.classList.add("visibility-show");
+  userOptionsElement.classList.remove("visibility-hidden");
+}
+
+function userOptionsClose() {
+  let userOptionsBackgroundElement = document.querySelector(".user-options");
+  userOptionsBackgroundElement.classList.add("opacity-hidden");
+
+  let userOptionsElement = document.querySelector(".user-options-content-general");
+  userOptionsElement.classList.add("visibility-hidden");
+}
+
+function enterLobby(enterLobbyNow){
+  enterLobbyNow.parentNode.classList.add("home-screen-visibility");
+
+  let contentGeneral = document.querySelector(".content-general-visibility");
+  contentGeneral.classList.add("content-general-to-visibility");
+}
