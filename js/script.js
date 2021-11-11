558c673919eb108bc017c02c9ce8fce155c6ae44
Rui Neto <ruineto11@gmail.com>
2021-11-11 04:18:17 -0300

    Projeto #05 - Bate-Papo UOL: Início com axios - carregar lobby e mantendo conexão do usuário

--- js/script.js
+++ js/script.js
@@ -22,3 +22,76 @@ function enterLobby(enterLobbyNow){
   let contentGeneral = document.querySelector(".content-general-visibility");
   contentGeneral.classList.add("content-general-to-visibility");
 }
+
+
+
+function nameUser() {
+  const userNameInput = document.querySelector(".home-screen-input");
+
+  const user = {
+    name: userNameInput.value,
+  }
+
+  const promisseUser = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", user);
+  promisseUser.then(addUser);
+
+  setInterval(keepConnection, 5000);
+}
+
+
+function keepConnection() {
+  const userNameInput = document.querySelector(".home-screen-input");
+
+  const user = {
+    name: userNameInput.value,
+  }
+
+  const promisseUser = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", user);
+  promisseUser.then(addUser);
+}
+
+function addUser(answerUser) {
+  console.log(answerUser);
+}
+
+
+const promisseMessagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+promisseMessagens.then(listMessages);
+let messages = [];
+
+function listMessages(answerMessages) {
+  messages = answerMessages.data;
+  console.log(messages);
+
+  const messagesContainer = document.querySelector(".container");
+
+  for(let i = 0; i < messages.length ; i++){
+    const message = messages[i];
+
+    if(message.type === "status"){
+      messagesContainer.innerHTML += `
+        <div class="lobby lobby-enter-leave">
+          <div class="lobby-content">
+            <!--current-time-->
+            <span class="current-time">${message.time}</span>
+
+            <!--lobby-user-name-->
+            <span class="lobby-user-name">${message.from}</span> ${message.text}
+          </div><!--lobby-content--> 
+        </div><!--lobby-->     
+      `;
+    }else{
+      messagesContainer.innerHTML += `
+      <div class="lobby">
+        <div class="lobby-content">
+          <!--current-time-->
+          <span class="current-time">${message.time}</span>
+      
+          <!--lobby-user-name-->                                    <!--lobby-message-everyone-->
+          <span class="lobby-user-name">${message.from}</span> para <span class="lobby-message-everyone">${message.to}</span>: ${message.text}
+        </div><!--lobby-content-->
+      </div><!--lobby-->   
+    `;
+    }
+  }
+}
