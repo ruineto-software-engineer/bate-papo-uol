be1def25ba0e21b5d560148ae29836bb91f70f32
Rui Neto <ruineto11@gmail.com>
2021-11-12 04:01:29 -0300

    Projeto #05 - Bate-Papo UOL: Chat, Mensagens, Scroll, Enter, Reservadamente e Listar usuários

--- js/script.js
+++ js/script.js
@@ -25,6 +25,35 @@ function enterLobby(enterLobbyNow){
 
 
 
+
+
+function chooseOnlineUser(choosedUser){
+  let choosedUserIconReplace = choosedUser.querySelector(".location-icon");
+  const iconCheck = document.querySelector(".user-options-select-users .user-options-checkmark");
+
+  if(iconCheck !== null){
+    iconCheck.remove();    
+  }
+
+  choosedUserIconReplace.innerHTML = `<i class="fa fa-check user-options-checkmark" aria-hidden="true"></i><!--user-options-checkmark-->`;
+}
+
+function chooseTypeMessage(typeMessage){
+  let typeMessageIconReplace = typeMessage.querySelector(".location-icon");
+  const iconCheck = document.querySelector(".user-options-messagens .user-options-checkmark");
+
+  if(iconCheck !== null){
+    iconCheck.remove();
+    typeMessageIconReplace.innerHTML = `<i class="fa fa-check user-options-checkmark" aria-hidden="true"></i><!--user-options-checkmark-->`;
+  }else{
+    typeMessageIconReplace.innerHTML = `<i class="fa fa-check user-options-checkmark" aria-hidden="true"></i><!--user-options-checkmark-->`;
+  }
+}
+
+
+
+
+
 function nameUser() {
   const userNameInput = document.querySelector(".home-screen-input");
 
@@ -32,12 +61,11 @@ function nameUser() {
     name: userNameInput.value,
   }
 
-  const promisseUser = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", user);
-  promisseUser.then(addUser);
+  axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", user);
 
   setInterval(keepConnection, 5000);
+  setInterval(listMessagesRequest, 3000);
 }
-
 
 function keepConnection() {
   const userNameInput = document.querySelector(".home-screen-input");
@@ -46,52 +74,173 @@ function keepConnection() {
     name: userNameInput.value,
   }
 
-  const promisseUser = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", user);
-  promisseUser.then(addUser);
-}
-
-function addUser(answerUser) {
-  console.log(answerUser);
+  axios.post("https://mock-api.driven.com.br/api/v4/uol/status", user);
 }
 
 
-const promisseMessagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promisseMessagens.then(listMessages);
-let messages = [];
+
+
+
+let messagesContainer;
+let lastMensagePick = "";
+
+function listMessagesRequest() {
+  const promisseMessagens = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+  promisseMessagens.then(listMessages);
+}
 
 function listMessages(answerMessages) {
+  const userNameInput = document.querySelector(".home-screen-input");
+
+  let messages = [];
   messages = answerMessages.data;
-  console.log(messages);
+  messagesContainer = document.querySelector(".container");
 
-  const messagesContainer = document.querySelector(".container");
+  if(messages[messages.length - 1].time === lastMensagePick.time){
+    return;
+  }
 
+  messagesContainer.innerHTML = "";
   for(let i = 0; i < messages.length ; i++){
     const message = messages[i];
 
     if(message.type === "status"){
       messagesContainer.innerHTML += `
-        <div class="lobby lobby-enter-leave">
-          <div class="lobby-content">
-            <!--current-time-->
-            <span class="current-time">${message.time}</span>
+      <div class="lobby lobby-enter-leave">
+        <span class="lobby-content lobby-message-width">
+          <!--current-time-->
+          <span class="current-time lobby-message-width">${message.time}</span>
 
-            <!--lobby-user-name-->
-            <span class="lobby-user-name">${message.from}</span> ${message.text}
-          </div><!--lobby-content--> 
-        </div><!--lobby-->     
+          <!--lobby-user-name-->
+          <span class="lobby-user-name lobby-message-width">${message.from}</span> <span class="lobby-message-width">${message.text}</span>
+        </span><!--lobby-content--> 
+      </div><!--lobby-->     
       `;
+    }else if(message.type === "private_message"){
+      if(message.to === userNameInput.value || message.from === userNameInput.value){
+        messagesContainer.innerHTML += `
+        <div class="lobby lobby-private">
+          <span class="lobby-content lobby-message-width">
+            <!--current-time-->
+            <span class="current-time lobby-message-width">${message.time}</span>
+  
+            <!--lobby-user-name-->                                                                       <!--lobby-message-private-->
+            <span class="lobby-user-name lobby-message-width">${message.from}</span> reservadamente para <span class="lobby-message-private lobby-message-width">${message.to}</span>: <span class="lobby-message-width">${message.text}</span>
+          </span><!--lobby-content-->
+        </div><!--lobby-->     
+        `;
+      }      
     }else{
       messagesContainer.innerHTML += `
       <div class="lobby">
-        <div class="lobby-content">
+        <span class="lobby-content lobby-message-width">
           <!--current-time-->
-          <span class="current-time">${message.time}</span>
+          <span class="current-time lobby-message-width">${message.time}</span>
       
-          <!--lobby-user-name-->                                    <!--lobby-message-everyone-->
-          <span class="lobby-user-name">${message.from}</span> para <span class="lobby-message-everyone">${message.to}</span>: ${message.text}
-        </div><!--lobby-content-->
+          <!--lobby-user-name-->                                                        <!--lobby-message-everyone-->
+          <span class="lobby-user-name lobby-message-width">${message.from}</span> para <span class="lobby-message-everyone">${message.to}</span>: <span class="lobby-message-width">${message.text}</span>
+        </span><!--lobby-content-->
       </div><!--lobby-->   
-    `;
+      `;
     }
   }
+
+  lastMensagePick = messages[messages.length - 1];
+
+  const lastMessage = document.querySelector('.container div:last-child');
+  lastMessage.scrollIntoView();
 }
+
+function sendMessage() {
+  const userNameInput = document.querySelector(".home-screen-input");
+  const messageInput = document.querySelector(".bottom-bar-input");
+
+  const checkmarkContact = document.querySelector(".user-options-select-users .user-options-checkmark");
+  const checkmarkMessage = document.querySelector(".user-options-messagens .user-options-checkmark");
+
+  let nameContact = checkmarkContact.parentNode.parentNode.children[0].innerText;
+  let optionMessage = checkmarkMessage.parentNode.parentNode.children[0].innerText;
+
+  if(nameContact === "Todos" && optionMessage === "Público"){
+    const sendMessageAll = {
+      from: userNameInput.value,
+      to: "Todos",
+      text: messageInput.value,
+      type: "message"
+    }
+
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", sendMessageAll);
+  }else if(nameContact !== "Todos" && optionMessage === "Reservadamente"){
+    const sendMessagePrivate = {
+      from: userNameInput.value,
+      to: nameContact,
+      text: messageInput.value,
+      type: "private_message"
+    }
+
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", sendMessagePrivate);
+  }else if(nameContact !== "Todos" && optionMessage === "Público"){
+    const sendMessageAllTo = {
+      from: userNameInput.value,
+      to: nameContact,
+      text: messageInput.value,
+      type: "message"
+    }
+
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", sendMessageAllTo);    
+  }
+}
+
+
+
+
+
+let usersContainer;
+let userSelect;
+
+function listOnlineUsersRequest() {
+  const promisseUsers = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+  promisseUsers.then(listOnlineUsers);
+}
+
+setInterval(listOnlineUsersRequest, 10000);
+function listOnlineUsers(answerUsers){
+  let users = [];
+  users = answerUsers.data;
+  usersContainer = document.querySelector(".user-options-select-users ul");
+
+  let optionAll = document.querySelector(".user-options-select-users ul li");
+  let iconCheck = document.querySelector(".user-options-select-users .user-options-checkmark");
+  let iconLiCheck = iconCheck.parentNode.parentNode;
+
+  userSelect = iconLiCheck.children[0].innerText;
+
+  usersContainer.innerHTML = "";
+  usersContainer.innerHTML = optionAll.outerHTML;
+  for(let i = 0; i < users.length ; i++){
+    const user = users[i];
+
+    if(userSelect == user.name){
+      usersContainer.innerHTML += iconLiCheck.outerHTML; 
+    }else{
+      usersContainer.innerHTML += `
+      <li class="user-flex-content" onclick="chooseOnlineUser(this)">
+        <div class="user-flex-li">
+          <ion-icon class="user-flex-icon" name="person-circle"></ion-icon><!--person-circle--> ${user.name}
+        </div>
+        <div class="location-icon"></div>
+      </li>
+      `;      
+    } 
+  }
+}
+
+
+
+
+
+document.addEventListener("keydown" , function (event) {
+  if (event.keyCode !== 13) return;
+  
+  sendMessage()
+})
